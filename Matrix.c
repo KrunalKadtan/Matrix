3f28202eaa00995eca564cbb3484ba7cd594ac02
Krunal Kadtan <krunalkadtan19@gmail.com>
2024-02-19 18:00:00 +0530

    Input Matrix Data

--- Matrix.c
+++ Matrix.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <conio.h>
 
+int row, column;
+
 int main()
 {
+    printf("Enter Number Of Rows : ");
+    scanf("%d", &row);
+    printf("Enter Numbers Of Columns : ");
+    scanf("%d", &column);
 
+    int Matrix[row][column];
+
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<column; j++)
+        {
+            printf("Enter A%d%d Element : ", i+1, j+1);
+            scanf("%d", &Matrix[i][j]);
+        }
+    }
 
     getch();
     return 0;
